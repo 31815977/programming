7afa18c84b23a6bca3bc5e5db7546862f0b45d5e
zhao-yu-sen <r0972841248@gmail.com>
2023-04-02 10:08:30 +0800

    test

--- 課程/20221202-python U7.py
+++ 課程/20221202-python U7.py
@@ -517,8 +517,3 @@ scores = [21,29,18,33,12,17,26,28,15,19]
 for count, score in enumerate(scores, 1):   # 初始值是 1
     if score >= 20:
         print(f"場次 {count} : 得分 {score}")
-        
-    
-
-
-
